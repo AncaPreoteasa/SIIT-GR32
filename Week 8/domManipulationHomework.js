14ac3a9240a11e29b38cdef543b78f4f674d0fd6
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2022-12-11 18:20:52 +0200

    Closure.

--- Week 8/domManipulationHomework.js
+++ Week 8/domManipulationHomework.js
@@ -1,111 +1,112 @@
-const ROCK_PAPER_SCISSORS_OPTIONS = ['Rock', 'Paper', 'Scissors'];
-let computerCounterWins = 0;
-let playerCounterWins = 0;
+(function() {
+    const ROCK_PAPER_SCISSORS_OPTIONS = ['Rock', 'Paper', 'Scissors'];
+    let computerCounterWins = 0;
+    let playerCounterWins = 0;
 
-document.getElementById("play-btn").addEventListener("click", showGame);
-document.getElementsByClassName("rock-image")[0].addEventListener("click", calculateAndDisplayWinnerforRock);
-document.getElementsByClassName("paper-image")[0].addEventListener("click", calculateAndDisplayWinnerforPaper);
-document.getElementsByClassName("scissors-image")[0].addEventListener("click", calculateAndDisplayWinnerforScissors);
+    document.getElementById("play-btn").addEventListener("click", showGame);
+    document.getElementsByClassName("rock-image")[0].addEventListener("click", calculateAndDisplayWinnerforRock);
+    document.getElementsByClassName("paper-image")[0].addEventListener("click", calculateAndDisplayWinnerforPaper);
+    document.getElementsByClassName("scissors-image")[0].addEventListener("click", calculateAndDisplayWinnerforScissors);
 
-function showGame() {
-    const introGameDiv = document.getElementById("intro-game");
-    introGameDiv.classList.add("hidden");
-    
-    const gameDiv = document.getElementById("game");
-    gameDiv.classList.remove("hidden");
-}
-
-function calculateAndDisplayWinnerforRock() {
-    const computerChoice = generateGameOption();
-    const hasComputerWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[1];
-    const hasUserWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[2];
-    const userChoice = ROCK_PAPER_SCISSORS_OPTIONS[0];
-
-    displayWinner(computerChoice, userChoice, hasComputerWon, hasUserWon);
-}
-
-function calculateAndDisplayWinnerforPaper() {
-    const computerChoice = generateGameOption();
-    const hasComputerWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[2];
-    const hasUserWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[0];
-    const userChoice = ROCK_PAPER_SCISSORS_OPTIONS[1];
-    displayWinner(computerChoice, userChoice,  hasComputerWon, hasUserWon);
-}
-
-function calculateAndDisplayWinnerforScissors() {
-    const computerChoice = generateGameOption();
-    const hasComputerWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[0];
-    const hasUserWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[1];
-    const userChoice = ROCK_PAPER_SCISSORS_OPTIONS[2];
-    displayWinner(computerChoice, userChoice, hasComputerWon, hasUserWon);
-}
-
-function generateGameOption() {
-    const randomNumber = Math.floor(Math.random() * ROCK_PAPER_SCISSORS_OPTIONS.length);
-    return ROCK_PAPER_SCISSORS_OPTIONS[randomNumber];
-}
-
-function displayWinner(computerChoice, userChoice, hasComputerWon, hasUserWon) {
-    let gameResultMessage; 
-    if (hasComputerWon) {
-        computerUpdateScore();
-        gameResultMessage = 'Computer wins!';
-    } else if (hasUserWon) {
-        playerUpdateScore();
-        gameResultMessage = 'User wins!';
-    } else {
-        gameResultMessage = "It's a draw!";
-        const drawSound = document.getElementById("draw-sound");
-        drawSound.play();
+    function showGame() {
+        const introGameDiv = document.getElementById("intro-game");
+        introGameDiv.classList.add("hidden");
+        
+        const gameDiv = document.getElementById("game");
+        gameDiv.classList.remove("hidden");
     }
 
-    const winnerAnnouncementElem = document.getElementById("winner-announcement");
-    if (winnerAnnouncementElem.firstChild) {
-        winnerAnnouncementElem.firstChild.remove();
+    function calculateAndDisplayWinnerforRock() {
+        const computerChoice = generateGameOption();
+        const hasComputerWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[1];
+        const hasUserWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[2];
+        const userChoice = ROCK_PAPER_SCISSORS_OPTIONS[0];
+
+        displayWinner(computerChoice, userChoice, hasComputerWon, hasUserWon);
     }
-    const textWinnerAnnouncement = document.createTextNode(gameResultMessage);
-    winnerAnnouncementElem.appendChild(textWinnerAnnouncement);
 
-
-
-    var explanationMessage;
-    if (hasComputerWon) {
-        explanationMessage = computerChoice + " beats " + userChoice + "!";
-    } else if (hasUserWon) {
-        explanationMessage = userChoice + " beats " + computerChoice + "!";
-    } else {
-        explanationMessage = userChoice + " matches " + computerChoice + "!";
+    function calculateAndDisplayWinnerforPaper() {
+        const computerChoice = generateGameOption();
+        const hasComputerWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[2];
+        const hasUserWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[0];
+        const userChoice = ROCK_PAPER_SCISSORS_OPTIONS[1];
+        displayWinner(computerChoice, userChoice,  hasComputerWon, hasUserWon);
     }
-    var winnerExplanationElement = document.getElementById("winner-explanation");
-    if (winnerExplanationElement.firstChild) {
-        winnerExplanationElement.firstChild.remove();
+
+    function calculateAndDisplayWinnerforScissors() {
+        const computerChoice = generateGameOption();
+        const hasComputerWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[0];
+        const hasUserWon = computerChoice === ROCK_PAPER_SCISSORS_OPTIONS[1];
+        const userChoice = ROCK_PAPER_SCISSORS_OPTIONS[2];
+        displayWinner(computerChoice, userChoice, hasComputerWon, hasUserWon);
     }
-    winnerExplanationElement.appendChild(document.createTextNode(explanationMessage));
 
-}
+    function generateGameOption() {
+        const randomNumber = Math.floor(Math.random() * ROCK_PAPER_SCISSORS_OPTIONS.length);
+        return ROCK_PAPER_SCISSORS_OPTIONS[randomNumber];
+    }
 
-function computerUpdateScore() {
-    computerCounterWins++;
-    const computerWinsParagraph = document.getElementById("counter-computer-winner");
-    const computeWinsTextNode = computerWinsParagraph.firstChild;
-    computeWinsTextNode.remove();
+    function displayWinner(computerChoice, userChoice, hasComputerWon, hasUserWon) {
+        let gameResultMessage; 
+        if (hasComputerWon) {
+            computerUpdateScore();
+            gameResultMessage = 'Computer wins!';
+        } else if (hasUserWon) {
+            playerUpdateScore();
+            gameResultMessage = 'User wins!';
+        } else {
+            gameResultMessage = "It's a draw!";
+            const drawSound = document.getElementById("draw-sound");
+            drawSound.play();
+        }
 
-    const newTextNode = document.createTextNode(computerCounterWins);
-    computerWinsParagraph.appendChild(newTextNode);
+        const winnerAnnouncementElem = document.getElementById("winner-announcement");
+        if (winnerAnnouncementElem.firstChild) {
+            winnerAnnouncementElem.firstChild.remove();
+        }
+        const textWinnerAnnouncement = document.createTextNode(gameResultMessage);
+        winnerAnnouncementElem.appendChild(textWinnerAnnouncement);
 
-    const userLosesSound = document.getElementById("lose-sound");
-    userLosesSound.play();
-}
 
-function playerUpdateScore() {
-    playerCounterWins++;
-    const playerWinsCounterParagraph = document.getElementById("counter-player-winner");
-    playerWinsCounterParagraph.firstChild.remove();
 
-    const newTextNode = document.createTextNode(playerCounterWins);
-    playerWinsCounterParagraph.appendChild(newTextNode);
+        var explanationMessage;
+        if (hasComputerWon) {
+            explanationMessage = computerChoice + " beats " + userChoice + "!";
+        } else if (hasUserWon) {
+            explanationMessage = userChoice + " beats " + computerChoice + "!";
+        } else {
+            explanationMessage = userChoice + " matches " + computerChoice + "!";
+        }
+        var winnerExplanationElement = document.getElementById("winner-explanation");
+        if (winnerExplanationElement.firstChild) {
+            winnerExplanationElement.firstChild.remove();
+        }
+        winnerExplanationElement.appendChild(document.createTextNode(explanationMessage));
 
-    const userWinsSound = document.getElementById("win-sound");
-    userWinsSound.play();
-}
+    }
 
+    function computerUpdateScore() {
+        computerCounterWins++;
+        const computerWinsParagraph = document.getElementById("counter-computer-winner");
+        const computeWinsTextNode = computerWinsParagraph.firstChild;
+        computeWinsTextNode.remove();
+
+        const newTextNode = document.createTextNode(computerCounterWins);
+        computerWinsParagraph.appendChild(newTextNode);
+
+        const userLosesSound = document.getElementById("lose-sound");
+        userLosesSound.play();
+    }
+
+    function playerUpdateScore() {
+        playerCounterWins++;
+        const playerWinsCounterParagraph = document.getElementById("counter-player-winner");
+        playerWinsCounterParagraph.firstChild.remove();
+
+        const newTextNode = document.createTextNode(playerCounterWins);
+        playerWinsCounterParagraph.appendChild(newTextNode);
+
+        const userWinsSound = document.getElementById("win-sound");
+        userWinsSound.play();
+    }
+})();
